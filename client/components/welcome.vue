2961538419ff4a14f04bc3aeda5f35464e8656e6
Riccardo Re <riccardo.re@clevermind.cloud>
2020-08-27 18:38:14 +0200

    Customizable welcome.vue
    Customizable welcome.vue based on administration configurations

--- client/components/welcome.vue
+++ client/components/welcome.vue
@@ -2,8 +2,8 @@
   v-app
     .onboarding
       .onboarding-content
-        img.animated.fadeIn(src='/_assets/svg/logo-wikijs.svg', alt='Wiki.js')
-        .headline.animated.fadeInUp {{ $t('welcome.title') }}
+        img.animated.fadeIn(:src='config.logoUrl', :alt='config.title')
+        .headline.animated.fadeInUp {{ config.title }}
         .subtitle-1.mt-3.animated.fadeInUp.wait-p1s {{ $t('welcome.subtitle') }}
         v-btn.mt-5.animated.fadeInUp.wait-p2s(color='primary', :href='`/e/` + locale + `/home`', x-large)
           v-icon(left) mdi-plus
@@ -11,6 +11,8 @@
 </template>
 
 <script>
+import _ from 'lodash'
+import gql from 'graphql-tag'
 
 export default {
   props: {
@@ -20,7 +22,31 @@ export default {
     }
   },
   data() {
-    return { }
+    return {
+      config: {
+        title: '',
+        logoUrl: ''
+      }
+    }
+  },
+  apollo: {
+    config: {
+      query: gql`
+        {
+          site {
+            config {
+              title
+              logoUrl
+            }
+          }
+        }
+      `,
+      fetchPolicy: 'network-only',
+      update: (data) => _.cloneDeep(data.site.config),
+      watchLoading (isLoading) {
+        this.$store.commit(`loading${isLoading ? 'Start' : 'Stop'}`, 'admin-site-refresh')
+      }
+    }
   }
 }
 </script>
